5eb270526282110103b3ec0039a283a07b908cde
awesomeroks <30774147+praveenVnktsh@users.noreply.github.com>
2021-06-06 14:22:14 +0530

    Update model.py

--- model.py
+++ model.py
@@ -8,16 +8,16 @@ class mainModel(nn.Module):
         self.dim = 512
 
         self.srcembd = nn.Embedding(vocab, self.dim)
-        self.stage1 = torch.nn.Transformer(d_model=self.dim)
-        self.stage2 = torch.nn.Transformer()
+        self.stage1 = nn.Transformer(d_model=self.dim)
+        self.stage2 = nn.Transformer()
         self.trgembd = nn.Linear(self.dim,vocab)
         self.relu = nn.ReLU()
 
     def forward(self, x,y):
         a = self.srcembd(x)
         b = self.srcembd(y)
-        out1 = self.stage1(a,b)
-        out2 = self.stage2(out1,b)
+        out1 = self.stage1(a, b)
+        out2 = self.stage2(out1, b)
         c = F.softmax(F.relu(self.trgembd(out1)),dim=2)
         d = F.softmax(F.relu(self.trgembd(out2)),dim=2)
         return {"stage1":c,"stage2":d}

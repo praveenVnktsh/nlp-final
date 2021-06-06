3fc82522a428db346800c3f45e95e4a385a5f63e
Harsh Patel <45335740+Harshp1802@users.noreply.github.com>
2021-06-06 14:48:26 +0530

    masks added

--- model.py
+++ model.py
@@ -12,12 +12,32 @@ class mainModel(nn.Module):
         self.stage2 = nn.Transformer()
         self.trgembd = nn.Linear(self.dim,vocab)
         self.relu = nn.ReLU()
+    
+    def make_len_mask(self, inp):
+            return (inp == 0).transpose(0, 1)
+            # Assuming <blank> stoi to be 0 for now!
 
-    def forward(self, x,y):
+    def forward(self, x, y):
+        # x-->input sentence
+        # y-->output sentence
+        
         a = self.srcembd(x)
         b = self.srcembd(y)
-        out1 = self.stage1(a, b)
-        out2 = self.stage2(out1, b)
+        src_pad_mask = self.make_len_mask(a)
+        trg_pad_mask1 = self.make_len_mask(b)
+
+        src_mask = self.stage1.generate_square_subsequent_mask(len(a)).to(x.device)
+        tgt_mask = self.stage1.generate_square_subsequent_mask(len(b)).to(x.device)
+        out1 = self.stage1(a, b, src_mask=src_mask, tgt_mask=tgt_mask, \
+            src_key_padding_mask=src_pad_mask, tgt_key_padding_mask=trg_pad_mask1, memory_key_padding_mask=src_pad_mask)
+        
+        src_mask = self.stage2.generate_square_subsequent_mask(len(out1)).to(y.device)
+        tgt_mask = self.stage1.generate_square_subsequent_mask(len(a)).to(x.device)
+        src_pad_mask = self.make_len_mask(out1)
+        trg_pad_mask1 = self.make_len_mask(a)
+        out2 = self.stage2(out1, a, src_mask=src_mask, tgt_mask=tgt_mask,\
+            src_key_padding_mask=src_pad_mask, tgt_key_padding_mask=trg_pad_mask1, memory_key_padding_mask=src_pad_mask)
+        
         c = F.softmax(F.relu(self.trgembd(out1)),dim=2)
         d = F.softmax(F.relu(self.trgembd(out2)),dim=2)
         return {"stage1":c,"stage2":d}
